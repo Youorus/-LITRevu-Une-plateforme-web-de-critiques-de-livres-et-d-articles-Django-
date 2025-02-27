70bf83fc332fff63991ec6f9c8e9382a76ab6bde
Marc <mtakoumba@gmail.com>
2025-02-27 14:27:34 +0100

    Ajout du model User

--- LITRevu-Project/settings.py
+++ LITRevu-Project/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'DjangoProject.urls'
+ROOT_URLCONF = 'LITRevu-Project.urls'
 
 TEMPLATES = [
     {
--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)  # Nom d'utilisateur unique
+    password = models.CharField(max_length=128)  # Stockage du mot de passe (hashé côté vue)
+    date_joined = models.DateTimeField(auto_now_add=True)  # Date d'inscription automatique
+
+    def __str__(self):
+        return self.username
