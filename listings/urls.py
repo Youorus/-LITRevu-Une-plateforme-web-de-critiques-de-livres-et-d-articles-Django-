064c4b4708ba03f922b612325f574f9d2dca27d9
Marc <mtakoumba@gmail.com>
2025-02-27 19:58:30 +0100

    implementation de la page de login et sa logique

--- listings/urls.py
+++ listings/urls.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from listings.views import index, register, home
+from listings.views import index, register, flux
 
 urlpatterns = [
     path("", index, name="index"),
     path("register", register, name="register"),
-    path("home", home, name="home"),
+    path("flux", flux, name="flux"),
+    path("logout", LogoutView.as_view(), name="logout"),
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from listings.forms import UserRegistrationForm
@@ -7,6 +8,21 @@ from listings.forms import UserRegistrationForm
 
 # Create your views here.
 def index(request):
+    """Page d'accueil qui affiche le formulaire de connexion si l'utilisateur n'est pas connecté"""
+    if request.user.is_authenticated:
+        return redirect("flux")  # Redirige vers /home si l'utilisateur est connecté
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("flux")  # Redirige vers /home après connexion
+        else:
+            messages.error(request, "Nom d'utilisateur ou mot de passe incorrect.")
+
     return render(request, "index.html")
 
 
@@ -18,7 +34,7 @@ def register(request):
             user = form.save()
             login(request, user)  # Connexion automatique après inscription
             messages.success(request, "Votre compte a été créé avec succès !")
-            return redirect("home")
+            return redirect("flux")
         else:
             messages.error(request, "Veuillez corriger les erreurs ci-dessous.")
     else:
@@ -26,5 +42,12 @@ def register(request):
 
     return render(request, "register_form.html", {"form": form})
 
-def home(request):
-    return render(request, "home.html")
+def logout(request):
+    """Gère la déconnexion des utilisateurs"""
+    logout(request)
+    messages.success(request, "Vous avez été déconnecté.")
+    return redirect("index")
+
+@login_required(login_url="/")
+def flux(request):
+    return render(request, "flux.html")
